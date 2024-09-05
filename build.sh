1d8b65db7758009d5f02488ddf61cd085b79b122
SuneetR <suneet38459@gmail.com>
2024-09-05 18:54:15 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@
 echo "Downloading GloVe embeddings..."
 curl -O http://nlp.stanford.edu/data/glove.6B.zip
 
-# Unzip the GloVe file
+# Unzip the GloVe file into a 'glove' directory
 echo "Unzipping GloVe embeddings..."
+mkdir -p glove
 unzip glove.6B.zip -d glove
 
 # Remove the zip file to save space
