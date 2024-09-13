ef373d69f8ef76343ce9f6584cf8999c87c6339a
SuneetR <suneet38459@gmail.com>
2024-09-13 10:27:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def analyze():
 
 if __name__ == '__main__':
     # Use the PORT environment variable provided by Render
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000, Render's default
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000
     app.run(host='0.0.0.0', port=port, debug=True)
